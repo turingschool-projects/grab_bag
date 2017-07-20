4f98c1c0b7b8fea3643f2ff83562bbf31649f9b8
Ilana Corson <ilana@turing.io>
2017-07-20 11:13:55 -0600

    add grab_bag

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170525040713) do
+ActiveRecord::Schema.define(version: 20170525030748) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,7 +45,6 @@ ActiveRecord::Schema.define(version: 20170525040713) do
     t.datetime "updated_at",             null: false
     t.integer  "permission", default: 0
     t.string   "slug"
-    t.string   "iv"
     t.integer  "status",     default: 0
     t.index ["folder_id"], name: "index_folders_on_folder_id", using: :btree
     t.index ["slug"], name: "index_folders_on_slug", using: :btree

a32204caf299dd48c1c48788f80a2f5ee2d6fc07
MasonHolland <masontholland@gmail.com>
2017-07-27 11:26:09 -0600

    added meta data endpoint and created test file

---
+++ spec/requests/api/v1/endpoints/metadata_records_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe "meta data records api" do
+  context "when the record exists" do
+    it "returns the current users meta data" do
+      user = create(:user)
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+      get 'api/v1/metadata'
+
+      result = JSON.parse(response.body)
+end

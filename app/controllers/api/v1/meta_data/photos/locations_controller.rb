82ad811de1f4d2c24c0be7fc9cb1e543e3d2b0f7
MasonHolland <masontholland@gmail.com>
2017-07-27 15:38:51 -0600

    adds functionality for rendering json for a users photos locations, passes in the lat and long in the form of an array of hashes

---
+++ app/controllers/api/v1/meta_data/photos/locations_controller.rb
@@ -0,0 +1,11 @@
+class Api::V1::MetaData::Photos::LocationsController < ApplicationController
+  def index
+    locations = MetaData.where(user_id: current_user.id).where.not(lat: nil)
+    @locs = []
+    locations.each do |loc|
+      @locs << {latitude: loc.lat, longitude: loc.long}
+      # @locs["data"].store(latitude: loc.lat, longitude: loc.long)
+    end
+    render json: @locs
+  end
+end

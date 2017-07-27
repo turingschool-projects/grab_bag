7611258ac7cd16709e74bb7de0d7a13257fb9a1d
NataliaColome <nmcolome@gmail.com>
2017-07-27 13:13:42 -0600

    call uploaded_photos_call in photos#index to pass all photos information and confirm total @photos.count

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,6 +1,6 @@
 class PhotosController < ApplicationController
   def index
-
+    @photos = uploaded_photos_call
   end
 
   def uploaded_photos_call
@@ -16,8 +16,9 @@ class PhotosController < ApplicationController
   def get_data_call(url)
     response = Faraday.get(url)
     response_json = JSON.parse(response.body, symbolize_names: true)
-    @photos = response_json[:data].map do |photo|
-      Faraday.get("https://graph.facebook.com/v2.10/#{photo[:id]}?fields=backdated_time,backdated_time_granularity,created_time,from,id,images,name,link,place,comments{like_count,comment_count,message,likes,from},tags{id,name},reactions{id,name,username,type}&access_token=#{ENV['my_fb_token']}").body
+    response_json[:data].map do |photo|
+      data_response = Faraday.get("https://graph.facebook.com/v2.10/#{photo[:id]}?fields=backdated_time,backdated_time_granularity,created_time,from,id,images,name,link,place,comments{like_count,comment_count,message,likes,from},tags{id,name},reactions{id,name,username,type}&access_token=#{ENV['my_fb_token']}")
+      JSON.parse(data_response.body, symbolize_names: true)
     end
   end
 end

68232f488ef07337ad8eb96c8b556261a3197139
MasonHolland <masontholland@gmail.com>
2017-07-27 11:30:16 -0600

    added comments to meta data request test for later revision

--- spec/requests/api/v1/endpoints/metadata_records_spec.rb
+++ spec/requests/api/v1/endpoints/metadata_records_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe "meta data records api" do
   context "when the record exists" do
     it "returns the current users meta data" do
       user = create(:user)
+      #create a meta data instance associated with user
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
 
       get 'api/v1/metadata'
@@ -12,6 +13,7 @@ RSpec.describe "meta data records api" do
 
       expect(response).to have_http_status(200)
       expect(result[])
+      #expect meta data to be present and called upon
     end
   end
 end

3bc33da665b20052c659908d1666529a801c2dd9
Carl Richmond <richmond.carl@outlook.com>
2017-07-28 09:59:52 -0600

    api endpoint topwords now returns top nouns and ajectives in anticipation of making a charts js pie graph on the dashboard view.

--- app/services/upload_logic_helper.rb
+++ app/services/upload_logic_helper.rb
@@ -4,27 +4,43 @@ class UploadLogicHelper
     @user = current_user
   end
 
-  def topword_adjective
-    list = MetaDataFile.where(user_id: user.id)
-    top_adjectives = extract_words(list)
-    top_words = rank_words(top_adjectives)
+  def topwords
+    topwords = {}
+    topwords[:adjectives] = topword_adjectives
+    topwords[:nouns] = topword_nouns
+    topwords
   end
+
 
   private
   attr_reader :user
 
-  def extract_words(list)
+  def topword_nouns
+    list = MetaDataFile.where(user_id: user.id)
+    top_nouns = extract_words(list, "top_nouns")
+    rank_words(top_nouns)
+  end
+
+  def topword_adjectives
+    list = MetaDataFile.where(user_id: user.id)
+    top_adjectives = extract_words(list, "top_adjectives")
+    rank_words(top_adjectives)
+  end
+
+  def extract_words(list, type)
     output = []
     list.each do |data|
-      words_array = data.top_adjective
+      words_array = []
+      if type == "top_adjectives"
+        words_array = data.top_adjective
+      elsif type == "top_nouns"
+        words_array = data.top_noun
+      end
       words_array.each do |word|
         output << word
       end
     end
     output
-  end
-
-  def extract_nouns(list)
   end
 
   def rank_words(words)

cff58d6d10448a54c4ca321662d8ea17b62fa9af
Carl Richmond <richmond.carl@outlook.com>
2017-07-29 09:01:21 -0600

    working on json response being the correct format for chart js

--- app/assets/javascripts/charts/verbage_pie_chart.js
+++ app/assets/javascripts/charts/verbage_pie_chart.js
@@ -1,17 +1,21 @@
 var ctx = document.getElementById('verbage_chart').getContext('2d');
-var data = $.getJSON('/api/v1/topwords', {codes: stripped_url})
+var data = $.ajax('/api/v1/file/adjectives');
+debugger
 var myChart = new Chart(ctx, {
-  type: 'line',
+  type: 'polarArea',
   data: {
-    labels: ['M', 'T', 'W', 'T', 'F', 'S', 'S'],
+    labels: ["M", "T", "W", "T", "F", "S", "S"],
     datasets: [{
-      label: 'apples',
-      data: [12, 19, 3, 17, 6, 3, 7],
-      backgroundColor: "rgba(153,255,51,0.4)"
-    }, {
-      label: 'oranges',
-      data: [2, 29, 5, 5, 2, 3, 10],
-      backgroundColor: "rgba(255,153,0,0.4)"
+      backgroundColor: [
+        "#2ecc71",
+        "#3498db",
+        "#95a5a6",
+        "#9b59b6",
+        "#f1c40f",
+        "#e74c3c",
+        "#34495e"
+      ],
+      data: [12, 19, 3, 17, 28, 24, 7]
     }]
   }
 });
--- spec/requests/api/v1/endpoints/topwords_spec.rb
+++ spec/requests/api/v1/endpoints/topwords_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 describe "can receive json information do" do
-  it "api endpoint returns information" do
+  xscenario "api endpoint returns information" do
     user = create(:user)
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
     folder = user.home
@@ -20,5 +20,23 @@ describe "can receive json information do" do
 
     expect(result_adjectives).to be_a Hash
     expect(result_nouns).to be_a Hash
+  end
+
+  scenario "api responds in correct format for chart js" do
+    user = create(:user)
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+    folder = user.home
+    binary = create :text_binary, folder: folder
+    yomu_1 = Yomu.new('app/assets/tests/comp-grade-standards.docx')
+    FileMetaDataService.new(yomu_1, user, binary.id).collect
+    yomu_2 = Yomu.new('app/assets/tests/Essay Format Template.docx')
+    FileMetaDataService.new(yomu_2, user, binary.id).collect
+    yomu_3 = Yomu.new('app/assets/tests/Overview_of_HighGrowth_Inc_Valuation.docx')
+    FileMetaDataService.new(yomu_3, user, binary.id).collect
+
+    get '/api/v1/file/adjectives'
+    result_adjectives = JSON.parse(response.body, :symbolize_names => true)
+
+    binding.pry
   end
 end

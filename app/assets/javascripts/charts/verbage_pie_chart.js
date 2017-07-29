a223c7075f147e6a73a8fcf17e8de7303f7b57c8
Carl Richmond <richmond.carl@outlook.com>
2017-07-29 10:40:42 -0600

    charts js works, but with more information than is needed

--- app/assets/javascripts/charts/verbage_pie_chart.js
+++ app/assets/javascripts/charts/verbage_pie_chart.js
@@ -2,26 +2,28 @@ $.ajax({
      url : ("/api/v1/file/adjectives"),
      type : 'GET',
      success : function(data) {
-        var json_data = JSON.parse(data);
-          debugger
+        var json_data = data;
+        var keys = Object.keys(json_data);
+        var values = Object.values(json_data);
+          var ctx = document.getElementById('verbage_chart').getContext('2d');
+          var myChart = new Chart(ctx, {
+            type: 'pie',
+            data: {
+              labels: keys,
+              datasets: [{
+                backgroundColor: [
+                  "#2ecc71",
+                  "#3498db",
+                  "#95a5a6",
+                  "#9b59b6",
+                  "#f1c40f",
+                  "#e74c3c",
+                  "#34495e"
+                ],
+                data: values
+              }]
+            }
+          });
+
     }
-});
-var ctx = document.getElementById('verbage_chart').getContext('2d');
-var myChart = new Chart(ctx, {
-  type: 'polarArea',
-  data: {
-    labels: ["M", "T", "W", "T", "F", "S", "S"],
-    datasets: [{
-      backgroundColor: [
-        "#2ecc71",
-        "#3498db",
-        "#95a5a6",
-        "#9b59b6",
-        "#f1c40f",
-        "#e74c3c",
-        "#34495e"
-      ],
-      data: [12, 19, 3, 17, 28, 24, 7]
-    }]
-  }
 });
